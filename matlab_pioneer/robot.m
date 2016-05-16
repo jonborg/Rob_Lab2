fd81830af972941d6decf9320c96ac1d871c9d94
rjri <rppguerra@gmail.com>
2016-05-16 10:06:26 +0100

    alteracoes

--- matlab_pioneer/robot.m
+++ matlab_pioneer/robot.m
@@ -11,32 +11,59 @@ if mode==1
     pioneer_set_controls(sp,round(vels_rob(1,i)*1000),round(vels_rob(2,i)*180/pi));
     start(t);
     while flag==0
-        pause(0.5);
-        sonars=pioneer_read_sonars()
-        [m,ind]=min(sonars(1:8));
-        if m<200 
+        m=6000
+        pause(1);
+        if i>45 && i<335
+            sonars=pioneer_read_sonars()
+            [m,ind]=min(sonars(1:8));
+        end
+        if m<300 
             stop(t);
-            delete(t);
-            if ind<=4
-                xref=xref+cos(teta_real(i)-pi/4);
-                yref=yref+sin(teta_real(i)-pi/4);
-            else
-                xref=xref+cos(teta_real(i)+pi/4);
-                yref=yref+sin(teta_real(i)+pi/4);
-            end
-            odom=pioneer_read_odometry();
-            xreal(i)=world(1);
-            yreal(i)=world(2);
-            teta_real(i)=-pi/2+2*pi/4096*odom(3);
-            if teta_real(i-1)<0 && odom(3)>3072
-               teta_real(i)=teta_real(i)-2*pi; 
-            end
-                        
-            erro_rob(:,i)=erro(xref(i),yref(i),teta_ref(i),xreal(i),yreal(i),teta_real(i));
-            v(:,i)=Controller(vel,wref(i),erro_rob(:,i));
-            vels_rob(:,i)=[vel*cos(erro_rob(3,i));wref(i)]-v(:,i);
-            t=timer('TimerFcn','odometria','StartDelay',T);
-            start(t);
+%             delete(t);
+              if (teta_real(i)<=-pi/4 && teta_real(i)>-3*pi/4)
+                if ind<=4 %esquerda do robo
+                    xref=xref-adjust;
+                else
+                    xref=xref+adjust;
+                end
+              end
+              if teta_real(i)<=pi/4 && teta_real(i)>-pi/4
+                if ind<=4 %esquerda do robo
+                    yref=yref-adjust;
+                else
+                    yref=yref+adjust;
+                end
+              end
+              if teta_real(i)<=3*pi/4 && teta_real(i)>pi/4
+                if ind<=4 %esquerda do robo
+                    xref=xref+adjust;
+                else
+                    xref=xref-adjust;
+                end
+              end
+              if (teta_real(i)<=5*pi/4 && teta_real(i)>3*pi/4 )||(teta_real(i)<=-3*pi/4) 
+                 if ind<=4 %esquerda do robo
+                    yref=yref+adjust;
+                else
+                    yref=yref-adjust;
+                end
+              end
+%             odom=pioneer_read_odometry();
+%             world=[0 1 xreal(1); -1 0 yreal(1); 0 0 1]*[odom(1)/1000 odom(2)/1000 1]';
+%             xreal(i)=world(1);
+%             yreal(i)=world(2);
+%             teta_real(i)=-pi/2+2*pi/4096*odom(3);
+%             if teta_real(i-1)<0 && odom(3)>3072
+%                teta_real(i)=teta_real(i)-2*pi; 
+%             end
+%                         
+%             erro_rob(:,i)=erro(xref(i),yref(i),teta_ref(i),xreal(i),yreal(i),teta_real(i));
+%             v(:,i)=Controller(vel,wref(i),erro_rob(:,i));
+%             vels_rob(:,i)=[vel*cos(erro_rob(3,i));wref(i)]-v(:,i);
+            %pioneer_set_controls(sp,round(vels_rob(1,i)*1000),round(vels_rob(2,i)*180/pi));
+%             t=timer('TimerFcn','odometria','StartDelay',T);
+%             start(t);
+            flag=1;
         end
         if flag==1
             odom=pioneer_read_odometry;
